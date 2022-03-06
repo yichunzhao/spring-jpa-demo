20393c1ffea8b2ab18780e3f7470bc16e5c982a2
yichunzhao <zhaoyichun@gmail.com>
2022-03-07 00:45:54 +0100

    removing springBootTest; and using sql statement to insert test data before test method, and rolling back the transaction afterwards.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Spring-JPA-Demo
 
-How to setup JPA-Hibernate with Spring DI, without SpringBoot
+How to set up JPA-Hibernate with Spring DI, without SpringBoot
 
-1.  It needs a database driver, a maven dependency, for this case, we use PostgreSQL.
-2.  In order to use JPA-Hibernate in this project, it is found that the easiest way is to include Spring Boot JPA starter directly.
-3.  It uses a Spring-Context to carry out DI.
-4.  It nees an extranl JAX binding maven dependency, for it is not a part of JDK anymore.
+1. It needs a database driver, a maven dependency, for this case, we use PostgreSQL.
+2. In order to use JPA-Hibernate in this project, it is found that the easiest way is to include Spring Boot JPA starter directly.
+3. It uses a Spring-Context to carry out DI.
+4. It needs an external JAX binding maven dependency, for it is not a part of JDK anymore.
+
+@ContextConfiguration defines class-level metadata that is used to determine how to load and configure an ApplicationContext for integration tests.
+
--- src/test/java/com/ynz/demo/jpa/BookRepositoryTest.java
+++ src/test/java/com/ynz/demo/jpa/BookRepositoryTest.java
@@ -3,26 +3,26 @@ package com.ynz.demo.jpa;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
 @ContextConfiguration(classes = {JPAConfiguration.class})
 @ExtendWith(SpringExtension.class)
 class BookRepositoryTest {
   @Autowired private BookRepository bookRepository;
 
   @Test
+  @Sql(
+      statements = "INSERT INTO book VALUES (1, 'my book')",
+      executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+  @Transactional
   void findByName() {
     var expected = "my book";
-    var book = new Book();
-    book.setName(expected);
-    bookRepository.save(book);
-
     var found = bookRepository.findByName(expected);
     assertTrue(found.isPresent());
     assertEquals(expected, found.get().getName());
